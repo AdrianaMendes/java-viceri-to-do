9160e75081482a39cdc020b5919700d22008e7b0
Adriana Mendes <adrianamirianmc@gmail.com>
2022-02-22 01:12:24 -0300

    feat: Integração do Spring Security com Swagger UI

---
+++ src/main/java/com/viceri/api/controllers/AuthenticationController.java
@@ -0,0 +1,24 @@
+package com.viceri.api.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viceri.api.dto.CredentialDto;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "Authentication")
+@RestController
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationController {
+	@ApiOperation("Login.")
+	@PostMapping("/login")
+	public void fakeLogin(@RequestBody CredentialDto dto) {
+		throw new IllegalStateException(
+				"This method shouldn't be called. It's implemented by Spring Security filters.");
+	}
+}
---
+++ src/main/java/com/viceri/api/dto/CredentialDto.java
@@ -0,0 +1,9 @@
+package com.viceri.api.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CredentialDto {
+	private String email;
+	private String password;
+}
